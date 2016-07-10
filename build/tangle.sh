926579305127a7f25fbe0ac8b1f84e416dba47a6
Holger Karl <hkarl@ieee.org>
2016-07-10 18:37:17 +0200

    supproitnng julatex output; generalizing path names

--- build/tangle.sh
+++ build/tangle.sh
@@ -6,29 +6,41 @@
 DIR=`pwd`
 FILES=""
 
+# on Holger's Mac: 
+EMACS="/Applications/Aquamacs 2.app/Contents/MacOS/Aquamacs"
+
+# in general, Linux: 
+# EMACS="emacs"
+
+# on Holger's Mac: 
+EMACSDIR="/Users/hkarl/lectures/gp1/installation/emacs.d"
+# in general: 
+# EMACSDIR="~/.emacs.d"
+
 # wrap each argument in the code required to call tangle on it
 for i in $@; do
     FILES="$FILES \"$i\""
 done
 
-emacs -Q --batch \
+"$EMACS" -Q --batch \
            --eval "(progn
      ; (add-to-list 'load-path (expand-file-name \"~/src/org/lisp/\"))∑
      ; (add-to-list 'load-path (expand-file-name \"~/src/org/contrib/lisp/\" t))
-     (add-to-list 'load-path (expand-file-name \"/home/jupyterhub/.emacs.d\"))
-     (add-to-list 'load-path (expand-file-name \"/home/jupyterhub/.emacs.d/elpa/org-20160704\"))
-     (add-to-list 'load-path (expand-file-name \"/home/jupyterhub/.emacs.d/elpa/json-mode-1.6.0\"))
-     (add-to-list 'load-path (expand-file-name \"/home/jupyterhub/.emacs.d/elpa/json-snatcher-20150511.2047\"))
-     (add-to-list 'load-path (expand-file-name \"/home/jupyterhub/.emacs.d/elpa/json-reformat-20160212.53\"))
+     (add-to-list 'load-path (expand-file-name \"${EMACSDIR}\"))
+     (add-to-list 'load-path (expand-file-name \"${EMACSDIR}/elpa/org-20160704\"))
+     (add-to-list 'load-path (expand-file-name \"${EMACSDIR}/elpa/json-mode-1.6.0\"))
+     (add-to-list 'load-path (expand-file-name \"${EMACSDIR}/elpa/json-snatcher-20150511.2047\"))
+     (add-to-list 'load-path (expand-file-name \"${EMACSDIR}/elpa/json-reformat-20160212.53\"))
      (require 'org)
      ; (require 'org-exp)
      (require 'ob)(require 'ob-tangle)
      (require 'json-mode)
      (require 'ox-juslides)
+     (require 'ox-julatex)
      (mapc (lambda (file)
             (find-file (expand-file-name file \"$DIR\"))
             (org-babel-tangle)
-            (org-latex-export-to-latex)
+            (org-julatex-export-to-file)
             (message (org-version))
             (org-juslides-export-to-file)
             (kill-buffer)) '($FILES)))" 2>&1
