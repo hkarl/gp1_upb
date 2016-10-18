5320e357296281b6d5c4228e3f8a0194c3d04a0e
Holger Karl <hkarl@ieee.org>
2016-10-18 17:27:09 +0200

    Also produce REMAINNG slides to be shown during class

--- content/groupby.py
+++ content/groupby.py
@@ -1,12 +1,22 @@
 import sys
+import itertools
+
 
 numvl = int(sys.argv[1])
 
 lines = sys.stdin.readlines()
+skiptext = sys.argv[2]
+if skiptext is not "":
+    lines = list(
+        itertools.dropwhile(lambda x: skiptext not in x,
+                                lines))[1:]
+
 numlines = len(lines)
 slidespervl = int(numlines/numvl)+1
 
 print(slidespervl)
+
+    
 
 for vl in range(numvl):
     print("=======================")
